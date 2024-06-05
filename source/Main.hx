21aff182503808254708a0ead4c6ac5a9f905bdd
Matthew <119807878+ParaMattKoopa@users.noreply.github.com>
2024-06-05 11:31:00 -0400

    Update Main.hx

--- source/Main.hx
+++ source/Main.hx
@@ -39,13 +39,13 @@ import haxe.io.Path;
 class Main extends Sprite
 {
 	var game = {
-		width: 1280, // WINDOW width
-		height: 720, // WINDOW height
+		width: 1920, // WINDOW width
+		height: 1080, // WINDOW height
 		initialState: TitleState, // initial game state
 		zoom: -1.0, // game state bounds
-		framerate: 60, // default framerate
+		framerate: 69, // default framerate
 		skipSplash: true, // if the default flixel splash screen should be skipped
-		startFullscreen: false // if the game should start at fullscreen mode
+		startFullscreen: true // if the game should start at fullscreen mode
 	};
 
 	public static var fpsVar:FPSCounter;
